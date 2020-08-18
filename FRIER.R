e4e51d2013f9872c43370d070041286500f81595
BioinformaticsBiophysicsUDE <daniel.hoffmann@uni-duisburg-essen.de>
2020-08-18 10:55:38 +0200

    Add files via upload

---
+++ FRIER.R
@@ -0,0 +1,45 @@
+require("csaw")
+require("GenomicAlignments")
+require("foreach")
+
+calculateEnrichedRanges <- function(bam, 
+                                    blacklist, minq=25,
+                                    window.width=150, bin.width=3000, filter=1, fraglen=60){
+
+  # initialize parameter
+  standard.chr <- paste0("chr", c(1:19, "X", "Y"))
+  blacklist <- readRDS(blacklist)
+  param <- readParam(minq=minq, discard=blacklist, restrict=standard.chr)
+  ## if no fraglen is provided, fraglength is calculated
+  if(length(fraglen) == 0){
+    x <- correlateReads(bam, param=reform(param, dedup=TRUE))
+    fraglen <- which.max(x) - 1
+    cat("calculated fragment length: ", fraglen, "\n")
+  }
+  ## get enriched regions
+  win.data <- windowCounts(bam, param=param, width=window.width, ext=fraglen, filter=filter)
+  #Filtering windows by abundance
+  # Count the number of extended reads overlapping a sliding window at spaced positions across the genome.
+  bins <- windowCounts(bam, bin=TRUE, width=bin.width, param=param) 
+  filter.stat <- filterWindows(win.data, bins, type="global")
+  bam2 <- readGAlignments(bam)
+  min.fc <- c(seq(1.1 ,16.1, by = 0.5))
+  
+  FRIER=foreach(i =  min.fc, .combine = "c") %do% {
+    keep <- filter.stat$filter > log2(i)
+    summay.keep = summary(keep)
+    #print(summay.keep)
+    #The actual filtering itself is done by simply subsetting the RangedSummarizedExperiment object.
+    filtered.data <- win.data[keep,]
+    retained.ranges <- filtered.data@rowRanges
+    retained.ranges.reduced <- reduce(retained.ranges)
+    #summarizeOverlaps -  simplifies counting reads in genomic ranges 
+    summary.overlap <- data.frame(intNotEmpty = 
+                                    assay(summarizeOverlaps(GRangesList(retained.ranges.reduced), bam2, 
+                                                            mode="IntersectionNotEmpty"), minoverlap=1, type = "any"))
+    summary.overlap$reads/length(bam2)
+  }
+  FRIER.m <- as.data.frame(t(matrix(c(min.fc,FRIER), nrow=2, byrow = T)))
+  names(FRIER.m) <- c("enrichmentLevel", "PercReads")
+  return(FRIER.m)
+}
---
+++ Spearman_Pearson_Correlation.R
@@ -0,0 +1,58 @@
+spearman_pearson_correlation <- function(file,  file1,  binsize, name, name1, blacklist.file, setLim= "auto", save=T, ext = 60){
+  library( ShortRead )
+  require("IRanges")
+  require("GenomicRanges")
+  require(csaw)
+  require(dplyr)
+  
+  standard.chr <- paste0("chr", c(1:19, "X", "Y"))
+  mm10_blacklist <-readRDS(blacklist.file)
+  param <- readParam( discard=mm10_blacklist, restrict=standard.chr)
+  bam.files <- c(file, file1)
+  ### get nb reads in bam files
+  countfile1 <- countBam(file1)
+  countfile1 <- countfile1$records
+  
+  countfile <- countBam(file)
+  countfile <- countfile$records
+  ### calculate bins
+  bins <- windowCounts(bam.files, bin=TRUE, width=binsize, param=param, filter=1, ext = ext)
+  coverage <- assay(bins)
+  coverage<- data.frame(coverage[,1]/(countfile/1000000), coverage[,2]/(countfile1/1000000))
+  names(coverage) <-c("A", "B")
+  ## calclate pearson and spearman correlation
+  correlation.p <- cor.test(coverage[,1], coverage[,2], method = "pearson")
+  corr.p.ul <- unlist(correlation.p)
+  correlation.s <- cor.test(coverage[,1], coverage[,2], method = "spearman")
+  corr.s.ul <- unlist(correlation.s)
+  
+  names.corr.p <- c(names(correlation.p)[-9], "conf.int1", "conf.int2")
+  correlation.p <-data.frame(names.corr.p, corr.p.ul)
+  names(correlation.p) <- c("A", "B")
+  names.corr.s <- c(names(corr.s.ul)[-7])
+  correlation.s <- data.frame(names.corr.s, corr.s.ul[-7])
+  names(correlation.s) <- c("A", "B")
+  
+  ## save correlation
+  correlation <- rbind(correlation.p, correlation.s)
+  save.name = paste(name, "-", name1, "_correlation.norm.cvs", sep="")
+  write.csv(correlation, save.name, row.names = F)
+  ## save image
+  ## save image
+  if(save){
+    if(setLim == "auto"){
+      save.name = paste(name, "-", name1, "_correlation.norm.png", sep="")
+      png(save.name)
+      plot(coverage[,1], coverage[,2], xlab= name, ylab= name1, cex=0.7, pch = 20)
+      dev.off()
+    }else{
+      save.name = paste(name, "-", name1, "_correlation.norm.png", sep="")
+      png(save.name)
+      plot(coverage[,1], coverage[,2], xlab= name, ylab= name1, xlim=c(0, setLim), 
+           ylim= c(0 , setLim), cex=0.7, pch = 20)
+      dev.off()
+    }
+  }
+  
+  return(correlation)
+}
